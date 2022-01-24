781198fc6c07fbd08fdd1e48b01b9fde35190772
Valentyn <sigano802@gmail.com>
2022-01-24 18:06:31 +0200

    i

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [value, setValue] = useState(0)
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        if (value > 0 && value % 3 === 0) {
+            console.log('Fizz')
+            setCount('Fizz')
+            if (value > 0 && value % 5 === 0) {
+                setCount("FizzBuzz")
+            }
+        } else if (value > 0 && value % 5 === 0) {
+            setCount('Buzz')
+        }
+    }, [value])
+
+    const plusHandler = () => {
+        setValue(value + 1)
+        setCount(value + 1)
+    }
+    const minusHandler = () => {
+        setValue(value - 1)
+        setCount(value - 1)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h1>{count}</h1>
+          <button onClick={minusHandler}>Minus</button>
+          <button onClick={plusHandler}>Plus</button>
+      </div>
   );
 }
 
